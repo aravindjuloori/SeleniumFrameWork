016515ffed27e608a13f162d13712f4c46b566d8
Aravind Juloori <julooriaravind@gmail.com>
2026-05-01 13:14:36 +0530

    new push to the new branch

--- src/test/java/pageObjects/BasePage.java
+++ src/test/java/pageObjects/BasePage.java
@@ -5,8 +5,8 @@ import org.openqa.selenium.support.PageFactory;
 
 public class BasePage {
 	
-	 WebDriver driver;
-	
+	public WebDriver driver;
+
 	public BasePage(WebDriver driver) {
 		this.driver=driver;
 		PageFactory.initElements(driver,this);
